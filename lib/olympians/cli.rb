fe16bce3cd7fe4c66927daf3e02c22929c267dea
Emily Lillie <emilylillie3@gmail.com>
2019-08-06 23:21:20 +0000

    finished hardcoding cli

--- lib/olympians/cli.rb
+++ lib/olympians/cli.rb
@@ -16,22 +16,27 @@ class Olympians
   end 
   
   def prompt_input
-    puts "Type in the number of the god you want to learn more about, or exit"
-    input = gets.strip
-    case input 
-      when "1"
-        puts "Aphrodite's symbols and whatever"
-      when "2"
-        puts "Ares's symbols and whatever"
-      when "3"
-        puts "Athena's symbols and whatever"
-      when "4"
-        puts "Hades's symbols and whatever"
-      when "5"
-        puts "Zeus's symbols and whatever"
+    input = "nil" #stub while loop
+    while input != "exit"
+      puts "Type in the number of the god you want to learn more about, or exit"
+      input = gets.strip
+      case input 
+        when "1"
+          puts "Aphrodite's symbols and whatever"
+        when "2"
+          puts "Ares's symbols and whatever"
+        when "3"
+          puts "Athena's symbols and whatever"
+        when "4"
+          puts "Hades's symbols and whatever"
+        when "5"
+          puts "Zeus's symbols and whatever"
+        else 
+          puts "I'm sorry, I don't know what you mean. Please type 1 through 5 or exit."
+      end
     end
   end 
     
   
-end 
-end 
+end #.CLI end
+end #.Olympians end
